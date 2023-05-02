100daf3aa07e3b0352bc8519fcc4cc36396e212e
Ujjwal Sahay <ujjwalsahayatwork@gmail.com>
2023-05-02 11:40:10 +0530

    using usememo in react

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ function App() {
   //1. state
   const [data, setData] = useState(0)
   const [data1, setData1] = useState(10)
+  const calculate = calculateValue(data)
+
 
 
   //2. function defination
-  let updateData = (e) =>{
+  let updateData = (e) => {
     console.warn("data update")
-    setData(data +1);
+    setData(data + 1);
   }
 
   let updateValue = (e) => {
     setData1(data1 + 2);
+  }
+
+  let calValue = (e) => {
+    setData((data) => data + 10);
   }
 
   //3. return
@@ -24,11 +30,19 @@ function App() {
     <>
       <h1>React react memo</h1>
       <h1> {data}</h1>
-      <button onClick={ (e)=>{ updateData(e)} }>data update</button>
-      <button onClick={ (e)=>{ updateValue(e) } }>value update</button>
-      <ReactMemo  value={data1}/>
+      <button onClick={(e) => { updateData(e) }}>data update</button>
+      <button onClick={(e) => { updateValue(e) }}>value update</button>
+      <ReactMemo value={data1} calValue={calValue} calculate={calculate} />
+
     </>
   );
+}
+
+const calculateValue = (num) => {
+  for (let index = 0; index < 10; index++) {
+    num += 10
+  }
+  return num
 }
 
 export default App;
--- src/ReactMemo.js
+++ src/ReactMemo.js
@@ -1,13 +1,17 @@
 import React, { memo } from 'react'
 
 function ReactMemo(props) {
+    debugger
     console.warn("value update")
-    
-    return(
+
+    return (
         <>
             <h1>{props.value}</h1>
+            <h1>calculate with useMemo</h1>
+            <button onClick={(e) => { props.calValue(e) }}>+</button>
+            {props.calculate}
         </>
     );
 }
 
-export default memo(ReactMemo)
+export default memo(ReactMemo);
